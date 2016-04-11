a699d0983280b1c123360c1970fcf1c9cb6f2aab
atpankurleo <atpankurleo@gmail.com>
2016-04-10 19:26:54 -0500

    setup,generate file name and avro setup

--- src/main/scala/edu/pa/Convolution.scala
+++ src/main/scala/edu/pa/Convolution.scala
@@ -3,13 +3,15 @@ package edu.pa
 import java.io.IOException
 
 import org.apache.avro.mapred.AvroKey
-import org.apache.avro.mapreduce.AvroMultipleOutputs
+import org.apache.avro.mapreduce.{AvroKeyOutputFormat, AvroJob, AvroMultipleOutputs}
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io.NullWritable
+import org.apache.hadoop.mapreduce.Job
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
 import org.apache.spark.SparkContext._
 import org.apache.spark.{SparkContext, SparkConf}
 
-import scala.collection.immutable.HashMap
+import scala.collection.mutable
 
 
 /**
@@ -17,10 +19,10 @@ import scala.collection.immutable.HashMap
   */
 object Convolution {
   val SIGNAL_BUFFER_SIZE: Int = 16777216
-  val KERNEL_START_FREQ: Short = 5
+  val KERNEL_START_FREQ: Int = 5
   val KERNEL_END_FREQ: Short = 200
   val KERNEL_WINDOW_SIZE: Int = 2001
-  private var kernelMap: HashMap[Integer, String] = null
+  private var kernelMap: mutable.HashMap[Integer, String] = null
   var kernelStack: Array[Array[Short]] = Array.ofDim(KERNEL_END_FREQ + 1, KERNEL_WINDOW_SIZE)
   private var n: Int = 0
   private var signal: Array[Float] = new Array[Float](SIGNAL_BUFFER_SIZE)
@@ -35,12 +37,17 @@ object Convolution {
   private var outkey: AvroKey[Rat] = new AvroKey[Rat](rat)
 
   def main(args: Array[String]) {
-    val conf = new SparkConf().setMaster("local[1]").setAppName("Neurohadoop scala")
-    val sc: SparkContext = new SparkContext(conf)
-    var lookup = sc.textFile("morlet-2000.dat")
+    //val conf = new SparkConf().setMaster("local[1]").setAppName("Neurohadoop scala")
+    val sparkConf = new SparkConf().setAppName("Neurohadoop scala")
+    val sc: SparkContext = new SparkContext(sparkConf)
+    //var lookup = sc.textFile("morlet-2000.dat")
+    var lookup = sc.textFile("hdfs:///neuro/lookup/morlet-2000.dat")
     var broadVar = sc.broadcast(lookup.collect())
-    println(broadVar.value)
-    var lines = sc.textFile("R192-2009-11-19-CSC10a.csv")
+    println(broadVar.value.length)
+    this.loadKernel(broadVar.value)
+    this.setup()
+    //var lines = sc.textFile("R192-2009-11-19-CSC10a.csv")
+    var lines = sc.textFile("hdfs:///neuro/input/R192-2009-11-19-CSC10a.csv")
     var partOneLines = sc.parallelize(lines.collect(), 1)
     System.out.println(partOneLines.take(5))
     var parseLines = partOneLines.map(x => new RatInputFormat().parse(x.toString()))
@@ -48,6 +55,37 @@ object Convolution {
     println(parseLines.collect())
     var populateArrays = parseLines.map(x => createTimeAndVoltageArray(x))
     println(populateArrays.take(5))
+    cleanup()
+  }
+
+  def ConvertStringArrayToShortArray(stringArray: Array[String]): Array[Short] = {
+    val shortArray: Array[Short] = new Array[Short](stringArray.length)
+      for (i <- 0 until stringArray.length) {
+        shortArray(i) = (stringArray(i)).toShort
+      }
+    return shortArray
+  }
+
+  def setup() {
+    fn = this.generateFileName("R192-2009-11-19-CSC10a.csv")
+    for (index <- KERNEL_START_FREQ to KERNEL_END_FREQ) {
+      var str:String = kernelMap.getOrElse(index, null)
+      println("index :" + index + "value: "+str)
+      kernelStack(index) = ConvertStringArrayToShortArray(str.split(","))
+    }
+  }
+
+  def loadKernel(broadval : Array[String]) {
+    var line: String = ""
+    var broad: Array[String] = broadval
+    var kernelFreq: Integer = KERNEL_START_FREQ
+    this.kernelMap = new mutable.HashMap[Integer, String]
+    for(i <- 0 until broadval.length ) {
+      this.kernelMap += (kernelFreq -> broad(i))
+      println(kernelFreq + "---- "+ broad(i))
+      kernelFreq += 1
+    }
+    println(kernelMap.size)
   }
 
   def createTimeAndVoltageArray(rec: RatInputFormat) {
@@ -68,6 +106,31 @@ object Convolution {
     }
   }
 
+  def generateFileName(fname: String):String = {
+    var ratnumber: String = null
+    var sessiondate: String = null
+    var channelid: String = null
+
+    var indexBegin: Int = 0
+    var indexEnd: Int = fname.indexOf('-')
+
+    ratnumber = fname.substring(indexBegin, indexEnd)
+    indexBegin = indexEnd + 1
+    indexEnd = fname.indexOf('-', indexBegin)
+    sessiondate = fname.substring(indexBegin, indexEnd)
+    indexBegin = indexEnd + 1
+    indexEnd = fname.indexOf('-', indexBegin)
+    sessiondate = sessiondate + '-' + fname.substring(indexBegin, indexEnd)
+    indexBegin = indexEnd + 1
+    indexEnd = fname.indexOf('-', indexBegin)
+    sessiondate = sessiondate + '-' + fname.substring(indexBegin, indexEnd)
+    indexBegin = indexEnd + 4
+    indexEnd = fname.indexOf('.', indexBegin)
+    channelid = fname.substring(indexBegin, indexEnd)
+
+    return "rat=" + ratnumber + "/dt=" + sessiondate + "/channel=" + channelid + "/" + ratnumber + "-" + sessiondate + "-" + channelid
+  }
+
   def cleanup() {
     val fft: FloatFFT_1D = new FloatFFT_1D(SIGNAL_BUFFER_SIZE / 2)
     try {
@@ -79,7 +142,7 @@ object Convolution {
       // tempTime = System.currentTimeMillis();
       fft.realForwardFull(signal)
 
-      for (k <- KERNEL_START_FREQ.toInt to KERNEL_END_FREQ ) {
+      for (k <- KERNEL_START_FREQ to KERNEL_END_FREQ ) {
         for (j <- 0 until KERNEL_WINDOW_SIZE) {
           kernel(j) = kernelStack(k)(j)
         }
@@ -99,16 +162,31 @@ object Convolution {
 
         var t: Int = KERNEL_WINDOW_SIZE - 1
 
+        println("Complex inverse completed")
+
+        var records = List()
         for (i <- (SIGNAL_BUFFER_SIZE / 2 - KERNEL_WINDOW_SIZE + 1) * 2 until (SIGNAL_BUFFER_SIZE / 2 - n) * 2 by -2) {
           rat.setTime(timestamp(t).toInt)
           rat.setFrequency(k.toInt)
           rat.setConvolution(Math.pow(kernel(i), 2).toFloat)
 
-          // context.write(outkey, NullWritable.get());
-          multipleOutputs.write("AVRO", outkey, NullWritable.get, fn)
+          println(rat.getTime + "--" + rat.getFrequency + "--" + rat.getConvolution)
+          //multipleOutputs.write("AVRO", outkey, NullWritable.get, fn)
           t += 1
+          //records = records :: rat
         }
       }
+
+      /*val records = sc.parallelize(Array(Rat))
+      val withValues = records.map((x) => (new AvroKey(x), NullWritable.get))
+
+      val conf = new Job()
+      FileOutputFormat.setOutputPath(conf, new Path(fn))
+      val schema = Rat.SCHEMA$
+      AvroJob.setOutputKeySchema(conf, schema)
+      conf.setOutputFormatClass(classOf[AvroKeyOutputFormat[Rat]])
+      withValues.saveAsNewAPIHadoopDataset(conf.getConfiguration)*/
+
     } catch {
       case ioe: IOException => {
         System.err.println(ioe.getMessage)
