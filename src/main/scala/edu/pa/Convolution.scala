81bb9decd260284e820128731265392e71ea8cb7
atpankurleo <atpankurleo@gmail.com>
2016-04-18 21:46:57 -0500

    added kryo serialization

--- src/main/scala/edu/pa/Convolution.scala
+++ src/main/scala/edu/pa/Convolution.scala
@@ -4,13 +4,16 @@ import java.io.IOException
 
 import org.apache.avro.mapred.AvroKey
 import org.apache.avro.mapreduce.{AvroKeyOutputFormat, AvroJob, AvroMultipleOutputs}
+import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io.NullWritable
 import org.apache.hadoop.mapred.JobConf
 import org.apache.hadoop.mapreduce.Job
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
 import org.apache.spark.SparkContext._
-import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.annotation.DeveloperApi
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{TaskContext, Partition, SparkContext, SparkConf}
 
 import scala.collection.mutable
 
@@ -26,35 +29,45 @@ object Convolution {
   private var kernelMap: mutable.HashMap[Integer, String] = null
   var kernelStack: Array[Array[Short]] = Array.ofDim(KERNEL_END_FREQ + 1, KERNEL_WINDOW_SIZE)
   private var nIndex: Int = 0
-  private val signal: Array[Float] = new Array[Float](SIGNAL_BUFFER_SIZE)
-  private val kernel: Array[Float] = new Array[Float](SIGNAL_BUFFER_SIZE)
-  private val timestamp: Array[Long] = new Array[Long](SIGNAL_BUFFER_SIZE)
+  private var signal: Array[Float] = new Array[Float](SIGNAL_BUFFER_SIZE)
+  private var kernel: Array[Float] = new Array[Float](SIGNAL_BUFFER_SIZE)
+  private var timestamp: Array[Long] = new Array[Long](SIGNAL_BUFFER_SIZE)
   private var lastTimestamp: Long = 0
   private var tempTime: Long = 0L
   private var fn: String = null
-  //private var multipleOutputs: AvroMultipleOutputs = null
   private var rat: Rat = new Rat
-  //private var outkey: AvroKey[Rat] = new AvroKey[Rat](rat)
-  var ratRecords: List[Rat] = List()
+  private var ratRecords: List[Rat] = List()
 
   def main(args: Array[String]) {
-    val sparkConf = new SparkConf().setMaster("local[1]").setAppName("Neurohadoop scala")
-    //val sparkConf = new SparkConf().setAppName("Neurohadoop scala")
-    sparkConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-    //MyKryoRegistrator.register(sparkConf)
+    //val sparkConf = new SparkConf().setMaster("local[1]").setAppName("Neurohadoop scala")
+    val sparkConf = new SparkConf().setAppName("Neurohadoop scala")
+    RatKryoRegistrator.register(sparkConf)
     val sc: SparkContext = new SparkContext(sparkConf)
 
-    var lookup = sc.textFile("morlet-2000.dat")
-    //val lookup = sc.textFile("hdfs:///neuro/lookup/morlet-2000.dat")
+    //var lookup = sc.textFile("morlet-2000.dat")
+    val lookup = sc.textFile("hdfs:///neuro/lookup/morlet-2000.dat")
     val broadVar = sc.broadcast(lookup.collect())
     println(broadVar.value.length)
     this.loadKernel(broadVar.value)
     this.setup()
 
-    val lines = sc.textFile("R192-2009-11-19-CSC10a.csv")
-    //val lines = sc.textFile("hdfs:///neuro/input/R192-2009-11-19-CSC10a.csv")
-      //.map(_.toString.split(",")).filter( _.length == 2).map(x => new RatInputFormat(x(0) , x(1)))
-    println("Input file count " + lines.count())
+    //val lines = sc.textFile("R192-2009-11-19-CSC10a.csv")
+    val lines = sc.textFile("hdfs:///neuro/input/R192-2009-11-19-CSC10a.csv")
+      .map(_.toString.split(",")).filter( _.length == 2)
+    var timestamps = lines.map(x => x(0).trim).collect()
+    println("timestamp count: " + timestamps.size)
+    var signals = lines.map(x => x(1).trim).collect()
+    println("Signals count: " + signals.size)
+
+    for(i <- 0 until timestamps.size) {
+      signal(i) = signals(i).toFloat
+      timestamp(i) = timestamps(i).toLong
+      nIndex += 1
+    }
+    signals = null
+    timestamps = null
+
+    /*println("Input file count " + lines.count())
     //val partOneLines = sc.parallelize(lines.collect())
     //System.out.println(partOneLines.take(5))
     var ratin: RatInputFormat  = new RatInputFormat()
@@ -65,7 +78,7 @@ object Convolution {
       }*/
       ratin.timestamp = values(0).trim
       ratin.voltage = values(1).trim
-      println("Parse :" + ratin)
+      println("Parselines :" + ratin)
       ratin
     }
     println("Parselines count: " + parseLines.count())
@@ -88,12 +101,15 @@ object Convolution {
         }
       }
     }
-    println("Populate Array :"+ populateArrays.count())
+    println("timestamp size :"+ timestamp.size)
+    for(i <- 0 until timestamp.length)println(timestamp(i))
+    println("signla size :" + signal.size)
+    println("Populate Array :"+ populateArrays.count())*/
     //println(populateArrays.take(5))
 
-    this.cleanup()
+    this.cleanup(sc)
 
-    val records = sc.parallelize(ratRecords)
+    val records = sc.parallelize(ratRecords, 1)
     val withValues = records.map((x) => (new AvroKey(x), NullWritable.get))
 
     val conf = new Job()
@@ -107,9 +123,9 @@ object Convolution {
 
   def ConvertStringArrayToShortArray(stringArray: Array[String]): Array[Short] = {
     val shortArray: Array[Short] = new Array[Short](stringArray.length)
-      for (i <- 0 until stringArray.length) {
-        shortArray(i) = (stringArray(i)).toShort
-      }
+    for (i <- 0 until stringArray.length) {
+      shortArray(i) = (stringArray(i)).toShort
+    }
     return shortArray
   }
 
@@ -128,14 +144,14 @@ object Convolution {
     this.kernelMap = new mutable.HashMap[Integer, String]
     for(i <- 0 until broadval.length ) {
       this.kernelMap += (kernelFreq -> broad(i))
-      println(kernelFreq + "---- "+ broad(i))
+      //println(kernelFreq + "---- "+ broad(i))
       kernelFreq += 1
     }
     println(kernelMap.size)
   }
 
   def createTimeAndVoltageArray(rec: RatInputFormat) {
-    println("---v= " +rec.getVoltage + " T= "+ rec.getTimestamp)
+    println("---v= " +rec.getVoltage + " T = "+ rec.getTimestamp)
     try {
       if (lastTimestamp > rec.getTimestamp) {
         throw new IOException("Timestamp not sorted at: " + lastTimestamp + " and " + rec.getTimestamp)
@@ -180,11 +196,11 @@ object Convolution {
     return "rat=" + ratnumber + "/dt=" + sessiondate + "/channel=" + channelid + "/" + ratnumber + "-" + sessiondate + "-" + channelid
   }
 
-  def cleanup() {
+  def cleanup(sc: SparkContext) {
     println("Starting Convolution")
     val fft: FloatFFT_1D = new FloatFFT_1D(SIGNAL_BUFFER_SIZE / 2)
     try {
-      println("nIndex before convolution : " + nIndex)
+      //println("nIndex before convolution : " + nIndex)
       for (index <- nIndex until SIGNAL_BUFFER_SIZE / 2) {
         signal(index) = 0
       }
@@ -215,18 +231,19 @@ object Convolution {
 
         println("Complex inverse completed")
 
-        println("Kernel Window size "+ KERNEL_WINDOW_SIZE)
+        //println("Kernel Window size "+ KERNEL_WINDOW_SIZE)
         println("NIndex value:" + nIndex)
+        //var ratList:List[Rat] = List()
         for (i <- (SIGNAL_BUFFER_SIZE / 2 - KERNEL_WINDOW_SIZE + 1) * 2 until (SIGNAL_BUFFER_SIZE / 2 - nIndex) * 2 by -2) {
           rat.setTime(timestamp(t).toInt)
-          rat.setFrequency(k.toInt)
+          rat.setFrequency(k.toShort)
           rat.setConvolution(Math.pow(kernel(i), 2).toFloat)
 
-          //println(rat.getTime + "--" + rat.getFrequency + "--" + rat.getConvolution)
-          //multipleOutputs.write("AVRO", outkey, NullWritable.get, fn)
           t += 1
           ratRecords = rat :: ratRecords
+          //ratList = rat :: ratList
         }
+        //ratRecords.++(sc.parallelize(ratList,1))
         println("Ratrecords count:" + ratRecords.size)
       }
 
---
+++ src/main/scala/edu/pa/RatKryoRegistrator.scala
@@ -0,0 +1,22 @@
+package edu.pa
+
+/**
+  * Created by Ankur on 4/18/2016.
+  */
+import com.esotericsoftware.kryo.Kryo
+
+import org.apache.spark.SparkConf
+import org.apache.spark.serializer.{KryoSerializer, KryoRegistrator}
+
+class RatKryoRegistrator extends KryoRegistrator {
+  override def registerClasses(kryo: Kryo) {
+    kryo.register(classOf[Rat])
+  }
+}
+
+object RatKryoRegistrator {
+  def register(conf: SparkConf) {
+    conf.set("spark.serializer", classOf[KryoSerializer].getName)
+    conf.set("spark.kryo.registrator", classOf[RatKryoRegistrator].getName)
+  }
+}
